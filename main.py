4dda4614a8e54b68f26d382c3ed11b62890fd3c3
Raagulbharatwaj <74106637+Raagulbharatwaj@users.noreply.github.com>
2022-07-01 19:11:18 +0530

    Added singly linked list with insert append and extend

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from pyds.graph.tree.binarytree import BinaryTree
+from pyds.llist import LinkedList
 
-b = BinaryTree(80)
-print(b)
-b.insert(40)
-print(b)
-b.insert(90)
-print(b)
-
+l = LinkedList(1)
+print(l)
+l.append(2)
+print(l)
+l.extend([3, 4])
+print(l)
+l.insert(0, 0)
+print(l)
+l.insert(5, 5)
+print(l)
+l.insert(100,3)
+print(l)
--- pyds/llist.py
+++ pyds/llist.py
@@ -3,21 +3,59 @@ from pyds.node import ListNode, DoubleListNode
 
 class LinkedList:
 
-    __root = None
-
     def __init__(self, x):
-        LinkedList.__root = ListNode(data=x)
+        self._head = ListNode(data=x)
+        self._len  = 1
+
+    def __getitem__(self, item):
+        if item == "head":
+            return self._head
+
+    def __str__(self):
+        temp = self._head
+        x = str(temp["data"])+" "
+        while temp["next"] is not None:
+            temp = temp["next"]
+            x += str(temp["data"])+" "
+        return x
 
     def append(self, x):
-        curr = LinkedList.__root
+        curr = self._head
         while curr["next"] is not None:
             curr = curr["next"]
         curr["next"] = ListNode(data=x)
+        self._len+=1
 
     def extend(self, x):
-        curr = LinkedList.__root
+        curr = self._head
         while curr["next"] is not None:
             curr = curr["next"]
         for i in x:
             curr["next"] = ListNode(data=i)
             curr = curr["next"]
+            self._len+=1
+
+    def insert(self, x, pos):
+        if pos > self._len:
+            raise RuntimeError(f"Unable to insert {x} at {pos} as length of the list object at 0x{id(self)} is {self._len}")
+        if pos == 0:
+            new = ListNode(data=x, next_=self._head)
+            self._head = new
+            self._len += 1
+        elif pos == self._len:
+            self.append(x)
+        else:
+            curr = self._head
+            count = 0
+            while count < pos-1 and curr["next"] is not None:
+                curr = curr["next"]
+                count += 1
+            new = ListNode(data=x, next_=curr["next"])
+            curr["next"] = new
+            self._len += 1
+
+
+class DoublyLinkedList:
+    def __init__(self, x):
+        self._head = DoubleListNode(data=x)
+
